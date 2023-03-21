c8b2cdad80731b220d318a1b44c2c4f772e5b572
Manjot Virdi <virdi_manjot@hotmail.com>
2023-03-21 16:34:37 -0700

    delete port 8000 variable

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const express = require("express");
 
 const app = express();
 
-const port = 8000;
-
 require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 
@@ -13,8 +11,6 @@ app.use(cors());
 // app.use(express.static("public"));
 app.use(express.json());
 
-
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
