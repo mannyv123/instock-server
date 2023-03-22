eb85e7912c08972bae099f62703faa9bc88de714
Manjot Virdi <virdi_manjot@hotmail.com>
2023-03-21 21:05:24 -0700

    add comments to server.js for readability; update endpoint to delete warehouse

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -1,24 +1,39 @@
+const { response } = require("express");
 const express = require("express");
 const router = express.Router();
 
 router.get("/api/warehouses", (req, res) => {
-  res.send("NOT IMPLEMENTED: returns a list of warehouses");
+    res.send("NOT IMPLEMENTED: returns a list of warehouses");
 });
 
 router.get("/api/warehouses/:id", (req, res) => {
-  res.send("NOT IMPLEMENTED: returns a specific warehouse");
+    res.send("NOT IMPLEMENTED: returns a specific warehouse");
 });
 
 router.post("/api/warehouses", (req, res) => {
-  res.send("NOT IMPLEMENTED: create a warehouse");
+    res.send("NOT IMPLEMENTED: create a warehouse");
 });
 
 router.put("/api/warehouses/:id", (req, res) => {
-  res.send("NOT IMPLEMENTED: update specific warehouse");
+    res.send("NOT IMPLEMENTED: update specific warehouse");
 });
 
-router.delete("/api/warehouses/:id", (req, res) => {
-  res.send("NOT IMPLEMENTED: delete a specific warehouse");
+//-----------Manjot Code Start------------------------
+
+//Endpoint to delete a warehouse
+router.delete("/warehouses/:id", (req, res) => {
+    const { warehouseId } = req.params;
+    knex("warehouses")
+        .where("id", warehouseId)
+        .del()
+        .then((response) => {
+            res.status(204).send("NOT IMPLEMENTED: delete a specific warehouse");
+        })
+        .catch((error) => {
+            res.status(404).send("Warehouse ID not found");
+        });
 });
+
+//-----------Manjot Code End--------------------------
 
 module.exports = router;
