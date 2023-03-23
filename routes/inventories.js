0ad129922a18a55bed64d91d6d59f4ff7dc9e97d
Manjot Virdi <virdi_manjot@hotmail.com>
2023-03-23 13:52:28 -0700

    section out code for feature

--- routes/inventories.js
+++ routes/inventories.js
@@ -2,23 +2,27 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/inventories", (req, res) => {
-  res.send("NOT IMPLEMENTED: returns a list of inventories");
+    res.send("NOT IMPLEMENTED: returns a list of inventories");
 });
 
 router.get("/inventories/:id", (req, res) => {
-  res.send("NOT IMPLEMENTED: returns a specific inventory");
+    res.send("NOT IMPLEMENTED: returns a specific inventory");
 });
+
+//----------------------------Manjot Code Start--------------------------------
 
 router.post("/inventories", (req, res) => {
-  res.send("NOT IMPLEMENTED: create an inventory");
+    res.send("NOT IMPLEMENTED: create an inventory");
 });
 
+//----------------------------Manjot Code End--------------------------------
+
 router.put("/inventories/:id", (req, res) => {
-  res.send("NOT IMPLEMENTED: update specific inventory");
+    res.send("NOT IMPLEMENTED: update specific inventory");
 });
 
 router.delete("/inventories/:id", (req, res) => {
-  res.send("NOT IMPLEMENTED: delete a specific inventory");
+    res.send("NOT IMPLEMENTED: delete a specific inventory");
 });
 
 module.exports = router;
