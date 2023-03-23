753681a37d99825bf6eef335bbac25c33ea66f24
Manjot Virdi <virdi_manjot@hotmail.com>
2023-03-23 14:37:43 -0700

    add inventory post route for new inventory

--- routes/inventories.js
+++ routes/inventories.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { v4: uuidv4 } = require("uuid");
 
 router.get("/inventories", (req, res) => {
     res.send("NOT IMPLEMENTED: returns a list of inventories");
@@ -11,8 +12,19 @@ router.get("/inventories/:id", (req, res) => {
 
 //----------------------------Manjot Code Start--------------------------------
 
-router.post("/inventories", (req, res) => {
-    res.send("NOT IMPLEMENTED: create an inventory");
+// router.post("/inventories", (req, res) => {
+//     res.send("NOT IMPLEMENTED: create an inventory");
+// });
+
+router.post("/inventories", async (req, res) => {
+    try {
+        req.body.id = uuidv4();
+        console.log(req.body);
+        // const result = await knex("inventories").insert(req.body);
+        // res.status(201).send(result[0]);
+    } catch (error) {
+        res.status(400).send(error);
+    }
 });
 
 //----------------------------Manjot Code End--------------------------------
